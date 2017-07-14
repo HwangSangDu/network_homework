855c8e1c8efbe514edfa38e322ea5c265f23aebf
HwangSangdu <ghkdtkden93@gmail.com>
2017-07-14 20:06:42 +0900

    memcpy error
    제가 직접 memcpy 를 만들도록 하겠습니다.

--- pcap_test.c
+++ pcap_test.c
@@ -1,35 +1,37 @@
- #include <pcap.h>
- #include <stdio.h>
- #include <string.h>
- #include <stdlib.h>
+#include <pcap.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 //eth.smac, eth.dmac / ip.sip, ip.dip / tcp.sport, tcp.dport / data
 typedef struct  packet_address
 {
-	/*
+	///*
 	u_char* eth[2];
 	u_char* ip[2];
 	u_char* port[2];
 	u_char* data[2];
 	//*/
+	/*
 	int eth[2];//start ,end
 	int ip[2];
 	int port[2];
 	int data[2];
+	//*/
 }Packet;
 
-u_char* my_strncpy(u_char* d,const u_char* s ,int len)
+u_char* my_strncpy(u_char* d, const u_char* s, int len)
 {
-	int i;
-	d = malloc(sizeof(u_char)*(len+1));
+	u_int i;
+	d = malloc(sizeof(u_char) * (len + 1));
 	for (i = 0; i < len; ++i)
 		d[i] = s[i];
 	d[len] = '\0';
 	return d;
 }
- int main(int argc, char *argv[])
- {
- 	Packet p;
- 	int i;
+int main(int argc, char *argv[])
+{
+	Packet p;
+	u_int i;
 	pcap_t *handle;			/* Session handle */
 	char *dev;			/* The device to sniff on */
 	char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
@@ -40,7 +42,7 @@ u_char* my_strncpy(u_char* d,const u_char* s ,int len)
 	struct pcap_pkthdr header;	/* The header that pcap gives us */
 	const u_char *packet;		/* The actual packet */
 
-	/* Define the device */
+								/* Define the device */
 	dev = pcap_lookupdev(errbuf);
 	if (dev == NULL) {
 		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
@@ -69,61 +71,60 @@ u_char* my_strncpy(u_char* d,const u_char* s ,int len)
 	}
 	/* Grab a packet */
 
-	while(1)
+	while (1)
 	{
 		packet = pcap_next(handle, &header);
-		///*
-		if(packet){
-			for (i = 0; i < 6; ++i)
-				printf("%0x" ,packet[i]);
-			printf("\n");
-		}
-
-		if(packet){
-			for (i = 6; i < 12; ++i)
-				printf("%0x" ,packet[i]);
-			printf("\n");
-		}
-
-		if(packet){
-			for (i = 26; i < 30; ++i)
-				printf("%0x" ,packet[i]);
-			printf("\n");
-		}
-
-		if(packet){
-			for (i = 30; i < 34; ++i)
-				printf("%0x" ,packet[i]);
-			printf("\n");
-		}
-
-		if(packet){
-			for (i = 34; i < 36; ++i)
-				printf("%0x" ,packet[i]);
-			printf("\n");
-		}
-		if(packet){
-			for (i = 36; i < 38; ++i)
-				printf("%0x" ,packet[i]);
-			printf("\n");
-		}
-		//*/
 		/*
 		printf("Jacked a packet with length of [%d]\n", header.len);
-		if(packet)//packet != NULL
+		if(packet){
+		for (i = 0; i < 6; ++i)
+		printf("%02x" ,packet[i]);
+		printf("\n");
+		}
+
+		if(packet){
+		for (i = 6; i < 12; ++i)
+		printf("%02x" ,packet[i]);
+		printf("\n");
+		}
+
+		if(packet){
+		for (i = 26; i < 30; ++i)
+		printf("%02x" ,packet[i]);
+		printf("\n");
+		}
+
+		if(packet){
+		for (i = 30; i < 34; ++i)
+		printf("%02x" ,packet[i]);
+		printf("\n");
+		}
+
+		if(packet){
+		for (i = 34; i < 36; ++i)
+		printf("%02x" ,packet[i]);
+		printf("\n");
+		}
+		if(packet){
+		for (i = 36; i < 38; ++i)
+		printf("%02x" ,packet[i]);
+		printf("\n");
+		}
+		//*/
+		///*
+		printf("Jacked a packet with length of [%d]\n", header.len);
+		if (packet)//packet != NULL
 		{
-			my_strncpy(p.eth[0] , packet , 6);
-			for (i = 0; i < (int)strlen(p.eth[0]); ++i)
-				printf("%x",p.eth[0][i]);
+			//my_strncpy(p.eth[0] , packet , 6);
+			//memcpy(p.eth[0], packet, 6);
+
+			for (i = 0; i < 6; ++i)
+				printf("%02x", p.eth[0][i]);
 			printf("\n");
 		}
 		//*/
-		/* Print its length */
-		
-		/* And close the session */
-		
 	}
 	pcap_close(handle);
 	return(0);
- }
+}
 
